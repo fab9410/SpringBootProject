576759dc1676ebddc0e7c0ef08d0f141c5f1fed0
fab9410 <r.rayan200310@gmail.com>
2024-10-04 11:28:08 +0200

    movie push

--- src/main/java/org/efrei/start/models/Movie.java
+++ src/main/java/org/efrei/start/models/Movie.java
@@ -1,11 +1,18 @@
 package org.efrei.start.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name="movie")
 public class Movie {
+
+
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -17,10 +24,27 @@ public class Movie {
     @OneToOne
     private Actor actor;
 
-    public String getId() {
+
+    public Movie(String title){
+        this.title = title;
+    }
+
+    public Movie() {}
+
+    public String getMovieName(){
+        return title;
+    }
+
+    public String getId(){
         return id;
     }
 
+    public void setMovieTitle(String title){
+        this.title = title;
+    }
 
+    public void setId(String id){
+        this.id = id;
+    }
 
 }
